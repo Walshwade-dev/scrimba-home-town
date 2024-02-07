326ce3306fbbb7be7f4ac6d2ddf712d156f3734e
Walshwade-dev <mainace8@gmail.com>
2024-02-07 17:32:22 +0300

    updated a responsive background image for hero section

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// tailwind.config = {
-//     theme: {
-//       extend: {
-//         colors: {
-//           clifford: '#da373d',
-//         }
-//       }
-//     }
-//   }
+const curvedTextOne = document.querySelector('#curved-text-1 p');
+const curvedTextTwo = document.querySelector('#curved-text-2 p');
+const curvedTextThree = document.querySelector('#curved-text-3 p');
+
+
+curvedTextOne.innerHTML = curvedTextOne.innerText.split('').map(
+    (char, i) =>   `
+        <span style="transform:rotate(${i * 7.2}deg)">${char}</span>
+    `
+).join('');
+
+curvedTextTwo.innerHTML = curvedTextTwo.innerText.split('').map(
+    (char, i) =>   `
+        <span style="transform:rotate(${i * 10.3}deg)">${char}</span>
+    `
+).join('');
+
+curvedTextThree.innerHTML = curvedTextThree.innerText.split('').map(
+    (char, i) =>   `
+        <span style="transform:rotate(${i * 16}deg)">${char}</span>
+    `
+).join('');
